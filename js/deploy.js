86545b23f3b2f42e3abfa3d1e5b8c59d32f4a961
Lucas <lucaspereiracunha08@gmail.com>
2018-07-14 15:01:37 -0700

    we have a string

---
+++ js/deploy.js
@@ -0,0 +1,37 @@
+web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
+VotingContract = web3.eth.contract(abi);
+// In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
+contractInstance = VotingContract.at('0x5b5eab6213e1579c555f5ee7eb5bea3efdb278c0');
+candidates = {"LUCAS": "candidate-1", "CARLOS": "candidate-2", "JOSE": "candidate-3", "LAURA": "candidate-4"};
+
+function voteForCandidate() {
+  // turns value in function to uppercase so input goes correctly on solidity code.
+  // $("#candidate").val(function(){
+  //   return this.value.toUpperCase();
+  // });
+
+  candidateName = $("#candidate").val();
+  contractInstance.voteForCandidate(candidateName, {from: web3.eth.accounts[0]}, function() {
+    let div_id = candidates[candidateName];
+    $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
+  });
+}
+
+function callNumberofVotes(){
+
+}
+
+$(document).ready(function() {
+  candidateNames = Object.keys(candidates);
+  for (var i = 0; i < candidateNames.length; i++) {
+    let name = candidateNames[i];
+    let val = contractInstance.totalVotesFor.call(name).toString();
+    $("#" + candidates[name]).html(val);
+  }
+});
+
+web3.eth.getTransactionReceipt("0x3b71af52004bf65647fb81451e6c9b6ad92fd149437c97580cddb21cb528403e", function(e, receipt) {const decodedLogs = abiDecoder.decodeLogs(receipt.logs);});
+
+
+farmInformation.deployed().then(function(instance) {return instance.greet.call()})
