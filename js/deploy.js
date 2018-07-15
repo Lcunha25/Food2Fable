37c22841bb4a832d5c0027acacd20aa61f3bac18
Lucas <lucaspereiracunha08@gmail.com>
2018-07-14 21:37:59 -0700

    working JS inputing information to smart contract

--- js/deploy.js
+++ js/deploy.js
@@ -1,37 +1,68 @@
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
-VotingContract = web3.eth.contract(abi);
-// In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance = VotingContract.at('0x5b5eab6213e1579c555f5ee7eb5bea3efdb278c0');
-candidates = {"LUCAS": "candidate-1", "CARLOS": "candidate-2", "JOSE": "candidate-3", "LAURA": "candidate-4"};
-
-function voteForCandidate() {
-  // turns value in function to uppercase so input goes correctly on solidity code.
-  // $("#candidate").val(function(){
-  //   return this.value.toUpperCase();
-  // });
-
-  candidateName = $("#candidate").val();
-  contractInstance.voteForCandidate(candidateName, {from: web3.eth.accounts[0]}, function() {
-    let div_id = candidates[candidateName];
-    $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
-  });
-}
-
-function callNumberofVotes(){
-
-}
-
-$(document).ready(function() {
-  candidateNames = Object.keys(candidates);
-  for (var i = 0; i < candidateNames.length; i++) {
-    let name = candidateNames[i];
-    let val = contractInstance.totalVotesFor.call(name).toString();
-    $("#" + candidates[name]).html(val);
-  }
-});
-
-web3.eth.getTransactionReceipt("0x3b71af52004bf65647fb81451e6c9b6ad92fd149437c97580cddb21cb528403e", function(e, receipt) {const decodedLogs = abiDecoder.decodeLogs(receipt.logs);});
-
-
-farmInformation.deployed().then(function(instance) {return instance.greet.call()})
+var abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_newgreeting",
+				"type": "string"
+			}
+		],
+		"name": "setGreeting",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_greeting",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getBlockNumber",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "greet",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+function setGreeting1(){
+    farmInfo = $("#farmInfo").val();
+    var _greeting = farmInfo ;
+    var farminformationContract = web3.eth.contract(abi);
+    var contractInstance = farminformationContract.at("0x3Bc9Aaa28670001A187aB1e616eCB825Eb2E7E88");
+    var transactionObj = {
+        from: '0xE71A1Cb389dc5B877E4c032492a2c83AC522460a'
+    }
+    contractInstance.setGreeting(_greeting, transactionObj, function(){
+        var test = contractInstance.greet();
+    })
+};
--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -1,5 +1,5 @@
 var farmInformation = artifacts.require("../contracts/farmInformation.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(farmInformation, "this is a test");
+  deployer.deploy(farmInformation, "WUHUUUU");
 };
